310e2d4e0da9e949d3950c73ab253bf9edbed3f9
harshil114 <harshilvispute114@gmail.com>
2024-06-28 19:14:24 +0530

    Set up project repository

---
+++ src/components/Body.js
@@ -0,0 +1,27 @@
+import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockData";
+import { useState } from "react";
+
+const Body = () => {
+  const [topRatedRestaurant, settopRatedRestaurant] = useState(resList);
+  const handleFilter = () => {
+    const filterList = resList.filter((res) => res.info.avgRating > 4.5);
+    settopRatedRestaurant(filterList);
+  };
+  return (
+    <div className="body">
+      <div className="filter">
+        <button className="filter-btn" onClick={handleFilter}>
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {topRatedRestaurant.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
