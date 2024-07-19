609ac93072d1c49474f08c21f4631930fe649e27
harshil114 <harshilvispute114@gmail.com>
2024-07-19 19:11:45 +0530

    feat - add feature to check online status of user

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RrestaurantMenu";
+import useOnlineStatus from "./utils/useOnlineStatus";
 
 const AppLayout = () => {
+  const onlineStatus = useOnlineStatus();
+
+  if (onlineStatus === false) return <h1>Hmm, looks you are offline...</h1>;
   return (
     <div className="app">
       <Header />
@@ -35,9 +39,9 @@ const appRouter = createBrowserRouter([
         element: <Contact />,
       },
       {
-        path:"/restaurant/:resID",
-        element:<RestaurantMenu />
-      }
+        path: "/restaurant/:resID",
+        element: <RestaurantMenu />,
+      },
     ],
     errorElement: <Error />,
   },
