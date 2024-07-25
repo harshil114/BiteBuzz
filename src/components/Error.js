8cb5951d8e30a5ed676c0660efed3c4eaedb8b22
harshil114 <harshilvispute114@gmail.com>
2024-07-25 15:38:04 +0530

    style error message

--- src/components/Error.js
+++ src/components/Error.js
@@ -4,7 +4,9 @@ const Error = () => {
   const err = useRouteError();
   return (
     <div>
-      <h3>Oops!! We will be back soon</h3>
+      <h3 className="error font-semibold text-center text-red-700 text-3xl mt-5">
+        Oops!! We will be back soon
+      </h3>
       <h3>
         {err.status} {err.statusText}
       </h3>
