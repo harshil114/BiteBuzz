ad7d8ce20d1f0d61f76d915b8cc0c3fd156ccce4
harshil114 <harshilvispute114@gmail.com>
2024-08-13 19:36:31 +0530

    style cart component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import ItemList from "./ItemList";
 import { clearCart } from "../utils/cartSlice";
+import emptyCartImg from "../assets/empty-cart.png";
 
 const Cart = () => {
   const cartItem = useSelector((store) => store.cart.items);
@@ -9,16 +10,21 @@ const Cart = () => {
     dispatch(clearCart());
   };
   return (
-    <div className="cart-container text-center m-4 p-4">
-      <h1 className="cart-heading text-2xl font-bold">Cart</h1>
-      <div className="item-list w-6/12 m-auto">
-        <button
-          className="btn-clear-cart p-2 m-2 bg-black text-white rounded-lg"
-          onClick={handleClearCart}
-        >
-          Clear Cart
-        </button>
-        {cartItem.length === 0 && <h1>Cart looks empty</h1>}
+    <div className="cart-container m-4 p-4">
+      <div className="item-list w-6/12 m-auto ">
+        {cartItem.length === 0 && (
+          <div className="img flex justify-center items-center">
+            <img src={emptyCartImg} alt="" />
+          </div>
+        )}
+        {cartItem.length !== 0 && (
+          <button
+            className="btn-clear-cart button mb-3  px-4 py-2 mx-4 bg-[#41d4d8] rounded-lg hover:bg-[#4adfe4] font-medium active:bg-[#41d4d8]"
+            onClick={handleClearCart}
+          >
+            Clear Cart
+          </button>
+        )}
         <ItemList items={cartItem} />
       </div>
     </div>
