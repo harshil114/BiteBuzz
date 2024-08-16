9dc97de177fa86f81c16e34c0478a17fb7fba9ad
harshil114 <harshilvispute114@gmail.com>
2024-08-16 19:01:09 +0530

    style error component

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,15 +1,41 @@
-import { useRouteError } from "react-router-dom";
+// import { useRouteError } from "react-router-dom";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.jpg";
 
 const Error = () => {
-  const err = useRouteError();
+  // const err = useRouteError();
   return (
     <div>
-      <h3 className="error font-semibold text-center text-red-700 text-3xl mt-5">
-        Oops!! We will be back soon
-      </h3>
-      <h3>
+      <div className="error-container-logo flex justify-end mt-2">
+        <Link to="/">
+          <img src={logo} alt="logo-img" className="logo h-8" />
+        </Link>
+      </div>
+      <div
+        className="container-data h-screen max-w-[430px] mx-auto pt-20"
+        style={{ color: " rgba(2, 6, 12, 0.75)" }}
+      >
+        <div className="error-text flex flex-col justify-center">
+          <h1 className="error-heading text-2xl font-bold mb-3">
+            Sorry, something went wrong.
+          </h1>
+          <div className="span flex flex-col font-normal text-lg">
+            <span className="mb-3">
+              We're working on getting this fixed as soon as we can.
+            </span>
+            <span className="mb-3">
+              <u>
+                <Link to="/">Go Back</Link>
+              </u>
+            </span>
+          </div>
+          <hr />
+        </div>
+
+        {/* <h3>
         {err.status} {err.statusText}
-      </h3>
+      </h3> */}
+      </div>
     </div>
   );
 };
